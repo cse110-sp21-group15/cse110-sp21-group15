e036e8f3708c58a287083e01c48182df4a81a1d1
Emily Jewik <35884912+ejewik@users.noreply.github.com>
2021-05-25 23:13:45 -0700

    Merging all actions work into pipeline-phase-1 (#24)
    This commit adds all the Github Action related changes regarding composite run step actions, and adds actions for linting, testing, and jsdocs.

--- pipeline/example_functions.js
+++ pipeline/example_functions.js
@@ -3,17 +3,17 @@ a couple simple functions to test Jest
 */
 
 const functions = {
-    multiple: (n1, n2) => n1*n2,
-    subtract: (n1, n2) => n1-n2,
-    add: (n1, n2) => n1+n2,
-    //example function that fail
-    divide_Will_Fail: (n1, n2) => n1 + n2 + 1,
-    makeAccount: () => {
-        const account = {
-            name: 'Zain',
-            age: 20
-        };
-        return account;
-    }
-}
+  multiple: (n1, n2) => n1 * n2,
+  subtract: (n1, n2) => n1 - n2,
+  add: (n1, n2) => n1 + n2,
+  // example function that fail
+  divide_Will_Fail: (n1, n2) => n1 + n2 + 1,
+  makeAccount: () => {
+    const account = {
+      name: 'Zain',
+      age: 20,
+    };
+    return account;
+  },
+};
 module.exports = functions;
