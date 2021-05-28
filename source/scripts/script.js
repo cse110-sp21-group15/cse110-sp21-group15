1a75bc172ac6c8f0ecdc30dbacc528ca24bfec21
rahuldadlani03 <rrdadlan@ucsd.edu>
2021-05-27 18:28:44 -0700

    Add functionality for menu button

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -6,6 +6,8 @@ const homeButton = document.getElementById('home-button');
 const logoButton = document.querySelector('nav > button');
 const featuresButton = document.getElementById('features-button');
 const downloadButton = document.getElementById('download-button');
+const menuButton = document.getElementById('menu-button');
+const navigationButtonList = document.getElementById('navigation-items');
 
 // When the back button is hit, set the state with the new page
 window.addEventListener('popstate', e => {
@@ -20,25 +22,40 @@ window.addEventListener('popstate', e => {
 homeButton.addEventListener('click', () => {
   console.log("Home clicked");
   router.setState('', false); // home state needs to be blank by default
+  hideMenuOptionsAfterClickingButtonIfMenuOpen()
 });
 
 // Go to home page when the logo is clicked
 logoButton.addEventListener('click', () => {
   console.log("Logo clicked");
   router.setState('', false); // home state needs to be blank by default
+  hideMenuOptionsAfterClickingButtonIfMenuOpen()
 });
 
 // Go to features page when features-button is clicked
 featuresButton.addEventListener('click', () => {
   console.log("Features clicked");
   router.setState('features', false);
+  hideMenuOptionsAfterClickingButtonIfMenuOpen()
 });
 
 // Go to features page when features-button is clicked
 downloadButton.addEventListener('click', () => {
   console.log("Features clicked");
   router.setState('download', false);
+  hideMenuOptionsAfterClickingButtonIfMenuOpen()
 });
+
+// Go to features page when features-button is clicked
+menuButton.addEventListener('click', () => {
+  navigationButtonList.classList.toggle("navigation-items-active");
+  navigationButtonList.classList.toggle("navigation-items-inactive");
+});
+
+function hideMenuOptionsAfterClickingButtonIfMenuOpen() {
+  navigationButtonList.classList.remove("navigation-items-active");
+  navigationButtonList.classList.add("navigation-items-inactive");
+}
 
 // Go to settings page when settings button is clicked
 // settings.addEventListener('click', () => {
