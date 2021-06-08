584529f8cd46dbf3a4e33fbdeac66f9c4b01dc82
v2yan <53798140+v2yan@users.noreply.github.com>
2021-06-08 12:14:26 -0700

    Future overview component (#50)
    * Add initial landing page
    
    The Feature page and routing between pages is completed
    
    * Fix router not pushing home state correctly
    
    * Add download hero component
    
    * Add animated BuJo graphic
    
    * Add basic testing file
    
    * Add functionality for menu button
    
    * Closes #40
    
    * formatted download page
    
    * adjust cropping
    
    * adjust cropping
    
    * Complete blue star (feature-component) component
    
    - Renamed aFeature to feature-component to follow proper naming conventions
    - Refactored feature-component to include all flavors of the component needed
    from Figma
    
    * Remove temporary styling from styles/style.css
    
    * Update feature-component to have additional styles
    
    * Remove extra elements that were used for testing
    
    * Make button on features page scroll to top
    
    * Make nav buttons scroll to top before navigation
    
    * Add component for row of features
    
    * Fix incorrect image offset in feature-component
    
    * Add large-features-component
    
    * Style class name to UpperCamelCase
    
    * Update classnames with UpperCamel when registering
    
    * add child.test.js and reorganize tests folders
    
    * Finish website
    
    * Fixed h1 font spelling
    
    * revised digital bujo animation
    
    * updated landing page to format animation
    
    * Fix typo for font name in style.css
    
    * Update first animation with new iteration
    
    * Create future overview component
    
    * Style future overview component
    
    * Fix spacing
    
    * Fix responsiveness
    
    * Delete rahul_test.js
    
    Co-authored-by: rahuldadlani03 <rrdadlan@ucsd.edu>
    Co-authored-by: Lutias <01joshua@gmail.com>
    Co-authored-by: dcullenUCSD <dcullen@ucsd.edu>
    Co-authored-by: Jonathan Zamora <50164418+jonzamora@users.noreply.github.com>
    Co-authored-by: Rahul Dadlani <44106252+rahuldadlani03@users.noreply.github.com>

---
+++ source/components/download-hero-component.js
@@ -0,0 +1,152 @@
+class DownloadHeroComponent extends HTMLElement {
+    constructor() {
+        super();
+
+        const template = document.createElement('template');
+
+        template.innerHTML = `
+          <style>
+            @font-face {
+              font-family: "Amaranth";
+              src: url(./Amaranth/Amaranth-Regular.ttf) format("truetype")
+            }
+            @font-face {
+              font-family: "Mulish";
+              src: url(./Mulish/static/Mulish-Regular.ttf) format("truetype")
+            }
+            .hero {
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+              flex-wrap: wrap;
+              padding-top: 100px;
+              padding-bottom: 60px;
+              background-color: #F0EBCC
+            }
+            .hero-logo-section {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              margin-left: 3.5%;
+              margin-bottom: 25px;
+            }
+            .logo-image {
+              width: 350px;
+              height: auto;
+            }
+            .hero-text-section {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              margin-left: 5%;
+              margin-right: 5%;
+            }
+            .hero-description {
+              max-width: 500px;
+              margin-bottom: 60px;
+            }
+            p.hero-description {
+              font-size: 30px;
+              font-family: Amaranth;
+              font-weight: bold;
+              color: #3F3697;
+              text-align: center;
+            }
+            p.hero-os-display {
+              font-family: "Mulish";
+              font-size: 20px;
+              text-align: center;
+              color: #3D84B8;
+            }
+            .download-button {
+              font-family: "Mulish";
+              font-weight: bold;
+              font-size: 26px;
+              text-align: center;
+              letter-spacing: -0.015em;
+              color: #F0EBCC;
+              background-color: #344FA1;
+              border-radius: 5px;
+              padding-left: 44px;
+              padding-right: 44px;
+              padding-top: 15px;
+              padding-bottom: 15px;
+              max-width: 350px;
+            }
+            button, input[type="submit"], input[type="reset"] {
+                /* Remove default styling */
+                background: none;
+                cursor: pointer;
+                border: none;
+                padding: 0;
+                outline: inherit;
+    
+                /* Add custom styling */
+                transition: background-color 0.5s;
+              }
+              button:hover {
+                color: #fffbe3;
+                background-color: #5f52d8;
+              }
+              button:active {
+                color: #fffbe3;
+                background-color: #5f52d8;
+              }
+          </style>
+  
+          <section class="hero">
+
+            <section class="hero-logo-section">
+                <img class="logo-image" 
+                    src="./media/JRNLlogo.svg"
+                    alt="JRNL"></img>
+            </section>
+            <section class="hero-text-section">
+                <p class="hero-description"></p>
+                <button class="download-button" type="button">Install JRNL</button>
+                <p class="hero-os-display"></p>
+            </section>   
+  
+          </section>
+          `;
+        this.attachShadow({ mode: 'open' })
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+        // Get attributes
+        const description = this.getAttribute('description');
+
+        // Set attributes
+
+        if (description) {
+            this.shadowRoot.querySelector('.hero-description').innerText = description;
+        } else {
+            this.shadowRoot.querySelector('.hero-description').innerText =
+                "Assignments, goals, habits, and more. Student life is tough to stay on top of and JRNL is here to help.";
+        }
+
+        // Detect user's OS (for displaying on the hero)
+        const userAgent = window.navigator.userAgent,
+            platform = window.navigator.platform,
+            macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+            windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+            iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+        let os = null;
+  
+        if (macosPlatforms.indexOf(platform) !== -1) {
+            os = "MacOS";
+        } else if (iosPlatforms.indexOf(platform) !== -1) {
+            os = "iOS";
+        } else if (windowsPlatforms.indexOf(platform) !== -1) {
+            os = "Windows";
+        } else if (/Android/.test(userAgent)) {
+            os = "Android";
+        } else if (!os && /Linux/.test(platform)) {
+            os = "Linux";
+        }
+        this.shadowRoot.querySelector('.hero-os-display').innerText = "For " + os;
+
+    }
+}
+
+customElements.define('download-hero-component', DownloadHeroComponent);
