c9a38d1f47154f98dfba56c7b5302ade352feb31
Emily Jewik <emilyjewik@gmail.com>
2021-06-10 18:46:18 -0700

    Test flask dummy backend

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php include_once("index.html"); ?>
-<?php include_once("/source/components"); ?>
-<?php include_once("/source/styles"); ?>
-<?php include_once("/source/scripts"); ?>
-<?php include_once("/source/media"); ?>
