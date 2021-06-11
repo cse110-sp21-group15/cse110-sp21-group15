6258d6ebc1b4bccab44d09ead19fe281398dcbd1
Emily Jewik <emilyjewik@gmail.com>
2021-06-10 18:54:14 -0700

    Change media include to src tag

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once("index.html"); ?>
+<?php include_once("/source/components"); ?>
+<?php include_once("/source/styles"); ?>
+<?php include_once("/source/scripts"); ?>
+<script src="/source/media" type="module"></script>
