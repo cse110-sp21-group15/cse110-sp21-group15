676988bee83a58bcaa47c015e28b672f62730452
Emily Jewik <emilyjewik@gmail.com>
2021-06-10 17:54:12 -0700

    Add source files to .php file

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php include_once("source/index.html"); ?>
+<?php include_once("index.html"); ?>
+<?php include_once("/source/components"); ?>
+<?php include_once("/source/styles"); ?>
+<?php include_once("/source/scripts"); ?>
+<?php include_once("/source/media"); ?>
