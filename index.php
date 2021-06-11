50be17d083bd894cfe062a4859ba1309f530e0ba
Emily Jewik <emilyjewik@gmail.com>
2021-06-10 18:04:01 -0700

    Change path of img in php file

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 <?php include_once("/source/components"); ?>
 <?php include_once("/source/styles"); ?>
 <?php include_once("/source/scripts"); ?>
-<?php include_once("/source/media"); ?>
+<?php include_once("/source/media/JRNLlogo.svg"); ?>
