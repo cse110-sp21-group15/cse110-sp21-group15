121cf089f7c3699f60151a1cf7961bfb6cbc722d
Lutias <01joshua@gmail.com>
2021-06-11 00:48:54 -0700

    adjusted formatting

--- source/components/menu-component.js
+++ source/components/menu-component.js
@@ -15,6 +15,18 @@ class MenuComponent extends HTMLElement {
                     src: url(./Mulish/static/Mulish-Regular.ttf) format("truetype")
                 }
 
+                ::-webkit-scrollbar {
+                    width: 7px;
+                }
+                ::-webkit-scrollbar-track {
+                    background: #DDDDDD; 
+                    border-radius: 7px;
+                }
+                ::-webkit-scrollbar-thumb {
+                    background: #3d84b8; 
+                    border-radius: 7px;
+                }
+
                 .hamburger-button {
                     /* Remove default styling */
                     background: none;
