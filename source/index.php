ec83a4f16417a03e7605b9387597f7caa8e5b2ee
Emily Jewik <emilyjewik@gmail.com>
2021-06-10 21:09:20 -0700

    Remove dummy php files

--- source/index.php
+++
@@ -1,6 +0,0 @@
-<?php include_once("index.html"); ?>
-<?php include_once("/source/components"); ?>
-<?php include_once("/source/styles"); ?>
-<?php include_once("/source/scripts"); ?>
-<!-- <script src="/source/media" type="module"></script> ->
-<?php include_once("/source/media"); ?>
